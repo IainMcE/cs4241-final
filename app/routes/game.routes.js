b9d588bd8c9179eb4303af6224053de3c5ab5b2f
gribsyrup <nsundararajan@wpi.edu>
2023-10-13 01:59:09 -0400

    All required API

--- app/routes/game.routes.js
+++ app/routes/game.routes.js
@@ -5,6 +5,10 @@ import {roundChallenge} from '../controllers/game.controller.js';
 import {init} from '../controllers/game.controller.js';
 import {status} from '../controllers/game.controller.js';
 import {gameSummary} from '../controllers/game.controller.js';
+import {join} from '../controllers/game.controller.js';
+import {placeBid} from '../controllers/game.controller.js';
+import {diceRoll} from '../controllers/game.controller.js';
+import {forfeit} from '../controllers/game.controller.js';
 
 
 const router = express.Router()
@@ -17,10 +21,15 @@ router.use(function(req, res, next) {
     next();
   });
 
-router.post("/api/game/roundWinner", roundWinner);
+router.get("/api/game/roundWinner", roundWinner);
 router.post("/api/game/roundChallenge", roundChallenge);
 router.post("/api/game/init", init);
+router.post("/api/game/placeBid", placeBid);
+router.post("/api/game/diceRoll", diceRoll);
+router.post("/api/game/forfeit", forfeit);
 router.get("/api/game/status", status);
 router.get("/api/game/summary", gameSummary);
+router.get("/api/game/join", join);
+
 
 export default router;
